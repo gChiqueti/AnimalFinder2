b285d54ed04497104cdb432b0bc7d7b97a360003
gChiqueti <36969020+gChiqueti@users.noreply.github.com>
2020-05-23 10:23:25 -0300

    Added CSS for nav bar and some general details

--- animal_finder/AnimalFinder2/settings.py
+++ animal_finder/AnimalFinder2/settings.py
@@ -133,8 +133,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
+# )
+
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "usuario/static"),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "usuario/static"),
+                    os.path.join(BASE_DIR, "main/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "main/media")
--- animal_finder/AnimalFinder2/urls.py
+++ animal_finder/AnimalFinder2/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,3 +25,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('', include('main.urls')),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
