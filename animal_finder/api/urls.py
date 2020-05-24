50ea7c13fa8a7009327225d3979b3e96e0e01188
gChiqueti <36969020+gChiqueti@users.noreply.github.com>
2020-05-24 10:22:48 -0300

    Fixed api to send correct animals status

--- animal_finder/api/urls.py
+++ animal_finder/api/urls.py
@@ -1,21 +1,27 @@
 from django.conf.urls import url, include
-from usuario.models import Dono
+from main.models import Animal
 from rest_framework import routers, serializers, viewsets
 
 # Serializers define the API representation.
-class DonoSerializer(serializers.ModelSerializer):
+class AnimalSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Dono
-        fields = ['id', 'nome', 'email', 'telefone']
+        model = Animal
+        fields = ['foto',
+                  'nome',
+                  'idade',
+                  'cidade_desaparecimento',
+                  'estado_desaparecimento',
+                  'status',
+                  'informacoes_extras']
 
 # ViewSets define the view behavior.
-class DonoViewSet(viewsets.ModelViewSet):
-    queryset = Dono.objects.all()
-    serializer_class = DonoSerializer
+class AnimalViewSet(viewsets.ModelViewSet):
+    queryset = Animal.objects.all()
+    serializer_class = AnimalSerializer
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'usuarios', DonoViewSet)
+router.register(r'animais', AnimalViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
